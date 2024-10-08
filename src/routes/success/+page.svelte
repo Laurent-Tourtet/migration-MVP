e5e04d4265a0583ba632aa37e042c4968407c41c
Laurent-Tourtet <laurent.tourtet@oclock.school>
2024-10-08 17:20:21 +0200

    ajout du lien d'ouverture de la boite mail de l'user

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -6,7 +6,10 @@
     <h1>Merci pour votre achat !</h1>
     <p>Votre abonnement a été activé avec succès.</p>
     <p>Un <span>email</span> vous à été envoyé à l'adresse fournie lors du paiement afin de créer votre mot de passe,</p>
-       <p><span>votre identifiant votre adresse mail</span></p>
+       <p><span>votre identifiant: votre adresse mail</span></p>
+       <p>
+        <a href="mailto:" id="access-mail">Accéder à vos mails</a>
+      </p>
   </main>
   <style>
     main {
@@ -34,5 +37,15 @@
       text-shadow: 2px 2px 4px #000000;
       font-weight: bold;
     }
+    a {
+    color: #007bff;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  
+  a:hover {
+    text-decoration: none;
+    color: #0056b3;
+  }
   </style>
   
