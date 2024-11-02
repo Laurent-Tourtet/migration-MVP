ec4eceab89b9351c0597489d6ac28c3385dae307
Laurent-Tourtet <laurent.tourtet@oclock.school>
2024-11-02 12:00:54 +0100

    modif page profile

---
+++ src/lib/components/ProfileInfos.svelte
@@ -0,0 +1,20 @@
+<aside class="left-menu">
+    <a href="/admin/profile">Profile</a>
+    {#if userInfos}
+<p>Hello {userInfos.first_name},</p>
+<ul>
+    <li>
+        <p>Nom et Prénom:{userInfos.first_name}  {userInfos.last_name}</p> <!-- Vérifiez la bonne propriété -->
+    </li>
+    <li>
+        <p>Email: {userInfos.email}</p> <!-- Vérifiez également ici -->
+    </li>
+    <li>
+        <p>Nombres de requêtes disponibles: {userInfos.requests_limit - userInfos.requests_made}</p>
+    </li>
+</ul>
+{:else}
+<p>Chargement des informations ou utilisateur non authentifié.</p>
+{/if}
+    <a href="/new-password">Reset Password</a>
+</aside>
--- src/routes/admin/profile/+page.svelte
+++ src/routes/admin/profile/+page.svelte
@@ -3,6 +3,7 @@
     import { fetchProfile } from '$lib/api'; // Importer la fonction fetchProfile
     // import { onMount } from 'svelte';
     import Header from '$lib/components/Header.svelte';
+    import ProfileInfos from '$lib/components/ProfileInfos.svelte';
 
     // onMount(() => {
     //     if (typeof window !== 'undefined') {
@@ -23,26 +24,7 @@
 </script>
 <Header />
 <main>
-    <aside class="left-menu">
-        <a href="/admin/profile">Profile</a>
-        {#if userInfos}
-    <p>Hello {userInfos.first_name},</p>
-    <ul>
-        <li>
-            <p>Nom et Prénom:{userInfos.first_name}  {userInfos.last_name}</p> <!-- Vérifiez la bonne propriété -->
-        </li>
-        <li>
-            <p>Email: {userInfos.email}</p> <!-- Vérifiez également ici -->
-        </li>
-        <li>
-            <p>Nombres de requêtes disponibles: {userInfos.requests_limit - userInfos.requests_made}</p>
-        </li>
-    </ul>
-{:else}
-    <p>Chargement des informations ou utilisateur non authentifié.</p>
-{/if}
-        <a href="/new-password">Reset Password</a>
-    </aside>
+    <ProfileInfos {userInfos} />
 <section>
     <h1>Profile</h1>
     
